74adb5873f880e89f9f8cec3aee34b0a71b86a3c
Michael Mulich <michael.mulich@gmail.com>
2013-03-22 03:10:02 -0700

    Developer orientation documentation to the design of gevent's usage in this system.

--- cnxrepo/events.py
+++ cnxrepo/events.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
-"""Application trigger event classes and handlers."""
+"""Application trigger event classes and handlers.
+
+Greenlets are (/need to be) set up in such a way that they mimic
+the transaction manager used in the request process. This is done using
+the ``coroutine`` decorator which will initialize a custom Greenlet class that
+sets up a new session (via the transaction manager)
+and in effect makes it transparent that we have even left the original
+process.
+
+The custom events defined in this module have been designed in such a way that
+they will lookup the SQLAlchemy model object originally handed to the event
+after the coroutine (and therefore using the new session) has been invoked.
+This avoids any DetachedInstanceErrors from being raised.
+
+"""
 from zope.interface import implementer
 from pyramid.threadlocal import get_current_registry
 
